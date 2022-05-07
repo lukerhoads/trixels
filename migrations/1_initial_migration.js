feb247e650aa5bf1ccec761f9b812b057854e27f
lukerhoads <pumpkineater661@gmail.com>
2022-05-07 01:15:50 -0500

    Trying makefile change

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,14 @@
 const Trixels = artifacts.require("Trixels");
-const TrixelsNFT = artifacts.require("TrixelsNFT");
+const TrixelsToken = artifacts.require("TrixelsToken");
 const TrixelsAuctionHouse = artifacts.require("TrixelsAuctionHouse");
 
-module.exports = function (deployer) {
-  deployer.deploy(Trixels);
-  deployer.deploy(TrixelsNFT);
-  deployer.deploy(TrixelsAuctionHouse);
+module.exports = async (deployer) => {
+  await deployer.deploy(Trixels);
+  // trixelsInstance = await Trixels.deployed();
+  // await trixelsInstance.initialize();
+  await deployer.deploy(TrixelsToken, "0x31b653c95756Aac850eA1e096f844e4478Aa8E97");
+  const trixelToken = await TrixelsToken.deployed();
+  await deployer.deploy(TrixelsAuctionHouse);
+  const trixelAuctionHouse = await TrixelsAuctionHouse.deployed();
+  await trixelAuctionHouse.initialize(trixelToken.address, 1000, 5, 10, "0x36811fA4e42C483F73F52D32960354Be023267dc");
 };
