0e8bf763eed86380cfca8a0912f46952d4c8df10
lukerhoads <pumpkineater661@gmail.com>
2022-06-05 23:00:43 -0500

    Added social icons

--- client/README.md
+++ client/README.md
@@ -5,5 +5,4 @@
 - Add gradient zooming to the zoom switch
 - Have grid move with scroll
 - Add mobile support
-- Make null checks inline
 - Add spot for social media
--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,9 @@ const config = {
   rpcUrl: 'http://127.0.0.1:8545',
   imageDimensions: 30,
   apiUrl: 'http://localhost:8080',
-  editTimeoutSeconds: 300
+  editTimeoutSeconds: 300,
+  facebookUser: '',
+  instagramUser: ''
 };
 
 export default config;
